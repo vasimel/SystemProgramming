443d542a2a1fa98ef1ab4b63ad1d2412718b061e
Vasilina Melnichenko <92578463+vasimel@users.noreply.github.com>
2023-10-29 16:26:34 +0300

    Update stash.c

--- stash.c
+++ stash.c
@@ -2,46 +2,61 @@
 #include <stdlib.h>
 #include <string.h>
 
+
+void saveFile(const char* filename, char* data, int size, const char* mode) {
+    FILE* file = fopen(filename, "r+");
+    if (file != NULL) {
+        fwrite(data, 1, 4, file);
+        fclose(file);
+        if (strcmp(mode, "distort") == 0) {
+            printf("File successfully distorted\n");
+        }
+        else if (strcmp(mode, "restore") == 0) {
+            printf("File successfully restored\n");
+        }
+        
+    } else {
+        printf("Error opening file for saving.\n");
+    }
+}
+
+
 int main(int argc, char *argv[])
 {
-    if (argc != 4)
+    if (argc != 3)
     {
-        fprintf(stderr, "Usage: %s mode (distort or restore) path/to/input patt/to/output\n", argv[0]);
+        fprintf(stderr, "Usage: %s mode (distort or restore) path/to/file\n", argv[0]);
         exit(1);
     }
     const char *command = argv[1];
     if (strcmp(command, "distort") != 0 && strcmp(command, "restore") != 0)
     {
-        fprintf(stderr, "Usage: %s mode (distort or restore) path/to/input patt/to/output\n", argv[0]);
+        fprintf(stderr, "Usage: %s mode (distort or restore) path/to/file\n", argv[0]);
         exit(1);
     }
 
-    const char *input = argv[2];
-    const char *output = argv[3];
+    const char *filename = argv[2];
+    
+    FILE* file = fopen(filename, "rb");
+    if (file != NULL) {
+        fseek(file, 0, SEEK_END);
+        int size = ftell(file);
+        fseek(file, 0, SEEK_SET);
 
-    const int buffer_size = 32;
-    char *buffer = (char*)malloc(buffer_size);
-    FILE *input_f = fopen(input, "rb");
-    FILE *output_f = fopen(input, "wb");
-    int i = 0;
-    while (!feof(input_f))
-    {
-        size_t count = fread(buffer, 1, buffer_size, input_f);
-        if (count == 0)
-            break;
-        if (i == 0) {
-            for (int j = 0; j < 4; j++) {
-                buffer[j] = buffer[j] ^ 0x11;
+        if (size >= 4) {
+            char data[4];
+            fread(data, sizeof(char), 4, file);
+            for (int i = 0; i < 4; i++) {
+                data[i] ^= 0x11;
             }
+            fclose(file);
+            saveFile(filename, data, 4, command);
+        } else {
+            printf("File size is less than 4 bytes.\n");
         }
-        i++;
-        fwrite(buffer, 1, count, output_f);
+    } else {
+        printf("Error opening file.\n");
     }
-    fclose(input_f);
-    fclose(output_f);
-
-    free(buffer);
 
     return 0;
 }
-
