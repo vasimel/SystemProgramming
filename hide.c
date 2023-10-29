8ef3ec8b7fa583c9e8bf67fb763ed2438a3618aa
Vasilina Melnichenko <92578463+vasimel@users.noreply.github.com>
2023-10-29 21:58:48 +0300

    Update hide.c

--- hide.c
+++ hide.c
@@ -15,21 +15,28 @@ int main(int argc, char *argv[]) {
 
     // Создаем скрытый каталог
     struct stat st = {0};
-    if (stat("./hidden_folder", &st) == -1) {
+    if (stat("./hidden_folder", &st) == -1 || !S_ISDIR(st.st_mode)) {
         mkdir("./hidden_folder", 0333);
+    }
+    else if (st.st_mode != strtol("0333", 0, 8)) {
+        chmod("./hidden_folder", strtol("0333", 0, 8));
     }
 
     // Получаем текущий путь (рабочую директорию)
     char *currentPath = getcwd(NULL, 1024);
+    char old_path[100];
+    char new_path[100];
 
-    // Создаем команду для переноса файла в скрытый каталог
-    char command[100];
-    sprintf(command, "mv %s/%s %s/hidden_folder/", currentPath, filename, currentPath);
+    sprintf(old_path, "%s/%s", currentPath, filename);
+    sprintf(new_path, "%s/hidden_folder/%s", currentPath, filename);
+    if (rename (old_path, new_path) == -1) {
+        printf("Unable to hide file %s\n", filename);
+    }
+    else {
+        printf("File %s hidden successfully\n", filename);
+    }
 
-    // Переносим файл в скрытый каталог
-    system(command);
 
-    printf("File %s hidden successfully\n", filename);
 
     return 0;
 }
